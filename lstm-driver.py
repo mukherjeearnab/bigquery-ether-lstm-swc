b6a3103976cc034bfb79daee0405fe090d1602f2
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-11-09 19:16:56 +0530

    LSTM Somewhat works

---
+++ lstm-driver.py
@@ -0,0 +1,251 @@
+# %% [markdown]
+# ## SET ENVIRONMENT VARIABLES
+
+# %%
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+from model import LSTM
+from sklearn.metrics import f1_score
+from torch.autograd import Variable
+import torch
+from sklearn.model_selection import train_test_split
+import json
+import pandas as pd
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+
+print(os.environ["CUDA_VISIBLE_DEVICES"])
+
+# %% [markdown]
+# ## Import and Prepare the DataSets
+
+# %% [markdown]
+# ### 1. Import Train & Test CSV(s)
+
+# %%
+
+# Set the TRAIN_SAMPLING_TECHNIQUE as follows
+# SMOTE ---> 'sm'
+# SMOTETomek ---> 'smt'
+# ADASYN ---> 'ada'
+TRAIN_SAMPLING_TECHNIQUE = 'sm'
+
+# Set the TEST_SAMPLING_TECHNIQUE as follows
+# RandomUnderSampler ---> 'rus'
+# TomekLinks ---> 'tmk'
+TEST_SAMPLING_TECHNIQUE = 'rus'
+
+# Importing Training Set
+train = pd.read_csv(f'./split_ds/opcode_{TRAIN_SAMPLING_TECHNIQUE}_TRAIN.csv')
+train['opcode'] = train['opcode'].apply(lambda x: json.loads(x))
+
+# Importing Testing Set
+test = pd.read_csv(f'./split_ds/opcode_{TEST_SAMPLING_TECHNIQUE}_TEST.csv')
+test['opcode'] = test['opcode'].apply(lambda x: json.loads(x))
+
+train = train.iloc[:10]
+print(train, test)
+
+
+# %% [markdown]
+# ### 2. Split the Test DataSet into Testing and Validiation DataSet
+
+# %%
+test, val = train_test_split(
+    test, test_size=0.5, random_state=69, shuffle=True, stratify=test['swc_label'])
+
+# %% [markdown]
+# ### 3. Convert DataFrame(s) to Numpy N-D Array(s)
+
+# %%
+
+
+def pandas2tensor(data, X=True):
+    tensor = Variable(torch.Tensor(data.tolist()))
+    if X:
+        return torch.reshape(tensor, (tensor.shape[0], 1, tensor.shape[1]))
+    else:
+        return torch.reshape(tensor, (tensor.shape[0], 1))
+
+
+# %%
+train_sequences = pandas2tensor(train['opcode'])
+train_labels = pandas2tensor(train['swc_label'], X=False)
+
+test_sequences = pandas2tensor(test['opcode'])
+test_labels = pandas2tensor(test['swc_label'], X=False)
+
+val_sequences = pandas2tensor(val['opcode'])
+val_labels = pandas2tensor(val['swc_label'], X=False)
+
+print("Train-Sequences", train_sequences.shape, type(train_sequences[0]))
+print("Train-Labels", train_labels.shape, type(train_labels[0]))
+
+print("Test-Sequences", test_sequences.shape, type(test_sequences[0]))
+print("Test-Labels", test_labels.shape, type(test_labels[0]))
+
+print("Validiation-Sequences", val_sequences.shape, type(val_sequences[0]))
+print("Validiation-Labels", val_labels.shape, type(val_labels[0]))
+
+
+# %% [markdown]
+# ## Create & Evaluate the Deep-Learning Model (RNN based on LSTM architecture)
+
+# %% [markdown]
+# ### 1. Define the Hyper-Parameters
+
+# %%
+OPCODE_SEQ_LEN = 1800
+EMBEDDING_DIM = 50
+NUM_EPOCS = 1024
+BATCH_SIZE = 128
+
+LEARNING_RATE = 0.001  # 0.001 lr
+INPUT_SIZE = 1800  # number of features
+HIDDEN_SIZE = 2  # number of features in hidden state
+NUM_LAYERS = 1  # number of stacked lstm layers
+NUM_CLASSES = 1  # number of output classes
+
+# %% [markdown]
+# ### 1A. Import Evaluation metrics
+
+# %%
+
+
+def f1(y_true, y_pred):
+    return f1_score(y_true, y_pred)
+
+
+def f1M(y_true, y_pred):
+    return f1_score(y_true, y_pred, average='macro')
+
+# %% [markdown]
+# ### 2. Define the Neural Network Structure (Layers)
+
+
+# %%
+
+model = LSTM(num_classes=NUM_CLASSES, input_size=INPUT_SIZE,
+             hidden_size=HIDDEN_SIZE, num_layers=NUM_LAYERS,
+             seq_length=train_sequences.shape[1])
+
+model.model
+
+# %% [markdown]
+# ### 3. Compile the model
+
+# %%
+model.compile(learning_rate=LEARNING_RATE)
+
+# %% [markdown]
+# ### 4. Fit and train the RNN model with Training and Validiation Data
+
+# %%
+
+# train_sequence = torch.tensor([train_sequences[0]])
+# train_label = torch.tensor([train_labels[]])
+
+model.fit(num_epochs=NUM_EPOCS, X=train_sequences, y=train_labels, batch_size=50,
+          X_validation=val_sequences, y_validation=val_labels)
+
+
+# %% [markdown]
+# ### 5. Evaluate performance of model using Testing DataSet
+
+# %%
+results = model.evaluate(test_sequences, test_labels, batch_size=BATCH_SIZE)
+print("Test Loss, Test Accuracy:", results)
+
+# %% [markdown]
+# ### 6. Save the model as HDF5 file
+
+# %%
+model.save(
+    f'./models/model_{TRAIN_SAMPLING_TECHNIQUE}_{TEST_SAMPLING_TECHNIQUE}_{NUM_EPOCS}.h5')
+
+# %%
+# Save History as Pickle
+with open(f'./models/history_{TRAIN_SAMPLING_TECHNIQUE}_{TEST_SAMPLING_TECHNIQUE}_{NUM_EPOCS}.pickle', 'wb') as fh:
+    pickle.dump(history.history, fh)
+
+# Save Results as Pickle
+with open(f'./models/results_{TRAIN_SAMPLING_TECHNIQUE}_{TEST_SAMPLING_TECHNIQUE}_{NUM_EPOCS}.pickle', 'wb') as fh:
+    pickle.dump(results, fh)
+
+# %% [markdown]
+# ### 7. Plot performance metrics of the Deep-Learning Model
+
+# %%
+sns.set()
+
+# %%
+# Accuracy Metrics
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.xlabel("Epochs")
+plt.ylabel('Accuracy')
+plt.legend(['accuracy', 'val_accuracy'])
+plt.show()
+
+# %%
+# Loss Metrics
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel("Epochs")
+plt.ylabel('Loss')
+plt.legend(['loss', 'val_loss'])
+plt.show()
+
+# %%
+pred_test_classes = model.predict_classes(
+    test_sequences, verbose=1, batch_size=128)
+pred_train_classes = model.predict_classes(
+    train_sequences, verbose=1, batch_size=128)
+
+
+print('Train Metrics\n-------------------------')
+# accuracy: (tp + tn) / (p + n)
+accuracy = accuracy_score(train_labels, pred_train_classes)
+print('Accuracy: %f' % accuracy)
+# precision tp / (tp + fp)
+precision = precision_score(train_labels, pred_train_classes)
+print('Precision: %f' % precision)
+# recall: tp / (tp + fn)
+recall = recall_score(train_labels, pred_train_classes)
+print('Recall: %f' % recall)
+# f1: 2 tp / (2 tp + fp + fn)
+f1 = f1_score(train_labels, pred_train_classes)
+print('F1 score: %f' % f1)
+f1M = f1_score(train_labels, pred_train_classes, average='macro')
+print('F1-Macro score: %f' % f1M)
+# confusion matrix
+matrix = confusion_matrix(train_labels, pred_train_classes)
+print(matrix)
+
+print('Test Metrics\n-------------------------')
+# accuracy: (tp + tn) / (p + n)
+accuracy = accuracy_score(test_labels, pred_test_classes)
+print('Accuracy: %f' % accuracy)
+# precision tp / (tp + fp)
+precision = precision_score(test_labels, pred_test_classes)
+print('Precision: %f' % precision)
+# recall: tp / (tp + fn)
+recall = recall_score(test_labels, pred_test_classes)
+print('Recall: %f' % recall)
+# f1: 2 tp / (2 tp + fp + fn)
+f1 = f1_score(test_labels, pred_test_classes)
+print('F1 score: %f' % f1)
+f1M = f1_score(test_labels, pred_test_classes, average='macro')
+print('F1-Macro score: %f' % f1M)
+# confusion matrix
+matrix = confusion_matrix(test_labels, pred_test_classes)
+print(matrix)
+
+# %%
--- model.py
+++ model.py
@@ -7,19 +7,24 @@ class LSTMNet(nn.Module):
     def __init__(self, num_classes: int, input_size: int, hidden_size: int, num_layers: int, seq_length: int):
         super(LSTMNet, self).__init__()
         self.num_classes = num_classes  # number of classes
-        self.num_layers = num_layers  # number of layers
+        self.num_layers = 1  # number of layers
         self.input_size = input_size  # input size
         self.hidden_size = hidden_size  # hidden state
         self.seq_length = seq_length  # sequence length
 
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, batch_first=True)  # lstm
+        self.embeddings = nn.Embedding(
+            150, 50, padding_idx=0)
+        self.lstm = nn.LSTM(input_size=50, hidden_size=hidden_size,
+                            num_layers=1, batch_first=True)  # lstm
         self.fc_1 = nn.Linear(hidden_size, 128)  # fully connected 1
         self.fc = nn.Linear(128, num_classes)  # fully connected last layer
 
         self.relu = nn.ReLU()
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+
+        x = self.embeddings(x.int())
+
         h_0 = Variable(torch.zeros(self.num_layers, x.size(0),
                                    self.hidden_size))  # hidden state
         c_0 = Variable(torch.zeros(self.num_layers, x.size(0),
@@ -28,12 +33,15 @@ class LSTMNet(nn.Module):
         # lstm with input, hidden, and internal state
         output, (hn, cn) = self.lstm(x, (h_0, c_0))
 
-        print(output)
+        # print(output)
 
         # reshaping the data for Dense layer next
         hn = hn.view(-1, self.hidden_size)
         out = self.relu(hn)
         out = self.fc_1(out)  # first Dense
+
+        # print("DenseInt", out)
+
         out = self.relu(out)  # relu
         out = self.fc(out)  # Final Output
         return out
@@ -52,25 +60,47 @@ class LSTM():
     def fit(self, num_epochs: int, X: torch.Tensor, y: torch.Tensor,
             batch_size: int, X_validation: torch.Tensor, y_validation: torch.Tensor):
 
-        # Divide into batches
-        X_batches = torch.split(X, batch_size)
-        y_batches = torch.split(y, batch_size)
+        # # Divide into batches
+        # X_batches = torch.split(X, batch_size)
+        # y_batches = torch.split(y, batch_size)
 
         for epoch in range(num_epochs):
-            for i, (X, y) in enumerate(zip(X_batches, y_batches)):
-                outputs = self.model.forward(X)  # forward pass
+            accuracy = 0.0
+            for (sequence, label) in zip(X, y):
+                outputs = self.model.forward(sequence)  # forward pass
                 self.optimizer.zero_grad()  # caluclate the gradient, manually setting to 0
 
                 # obtain the loss function
-                loss = self.criterion(outputs, y)
+                loss = self.criterion(outputs, label)
 
                 loss.backward()  # calculates the loss of the loss function
 
                 self.optimizer.step()  # improve from loss, i.e backprop
 
-                print(
-                    f'Epoch {epoch} of {num_epochs}. Batch {i} of {len(X_batches)}. Loss={loss.item()} \r')
+                # print(outputs, y)
+
+                output = (outputs > 0.5).float()
+                accuracy += (output == label)
+
+            print(X.shape)
+
+            accuracy = accuracy/y.shape[0]  # self.__evaluate(X=X, y=y)
+
+            print(
+                f'Epoch {epoch} of {num_epochs}. Loss={loss.item()} Acc={accuracy}', end='\n')
 
     # def __accuracy(predictions, labels):
     #     classes = torch.argmax(predictions, dim=1)
     #     return torch.mean((classes == labels).float())
+
+    def __evaluate(self, X: torch.Tensor, y: torch.Tensor, use_cuda=False):
+        self.model.eval()
+        with torch.no_grad():
+            acc = .0
+            if use_cuda:
+                X = X.cuda()
+                y = y.cuda()
+            y_preds = self.model(X)
+            y_preds = (y_preds > 0.5).float()
+            acc = (y_preds == y).sum()/float(y_preds.shape[0])
+        return acc.detach().item()
